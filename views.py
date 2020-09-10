65dcf384d43c1c3abba2182e0232acdb45fc76fd
gusisong <345354880@qq.com>
2020-09-10 21:49:50 +0800

    none

--- views.py
+++ views.py
@@ -15,7 +15,7 @@ app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 app.config['SECRET_KEY'] = '986v3n90d567cxn95n76vg74m'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
