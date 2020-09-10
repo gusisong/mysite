fb7491280b4a395a72ff99b2a697af46b331588e
GuSisong <345354880@qq.com>
2020-09-10 13:52:30 +0800

    xxx

--- views.py
+++ views.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, TextAreaField
+from wtforms.validators import Required
 import csv
 
 app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = '986v3n90d567cxn95n76vg74m'
 
 
 class UploadForm(FlaskForm):
     file = FileField('Upload', validators=[FileRequired()])
+    submit = SubmitField('Submit')
+
+
+class TextForm(FlaskForm):
+    content = TextAreaField('正文内容', validators=[Required()])
+    signature = TextAreaField('邮件签名', validators=[Required()])
     submit = SubmitField('Submit')
 
 
@@ -41,9 +48,20 @@ def upload():
         file_name = form.file.data.filename
         file_path = 'uploads/' + file_name
         form.file.data.save(file_path)
-        status = '文件上传成功: ' + file_name
-        return render_template('upload.html', form=form, upload_status=status)
-    return render_template('upload.html', form=form)
+        flash('文件上传成功：' + file_name)
+        return redirect(url_for('upload'))
+    return render_template('upload.html', uploadform=form)
+
+
+@app.route('/text/', methods=['GET', 'POST'])
+def text():
+    form = TextForm()
+    if form.validate_on_submit():
+        session['content'] = form.content
+        session['signature'] = form.signature
+        return redirect(url_for('text'))
+    return render_template('text.html', textform=form, content=session.get('content'),
+                           signature=session.get('signature'))
 
 
 if __name__ == '__main__':
